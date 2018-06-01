7b302c4e274bf276d3fb3434a8084efd009e3a1e
Darío Villena <ILoveYouDrZaius>
2018-06-01 20:04:37 +0200

    Cambios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Note } from './Note';
+import { NoteService } from './note-service.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  title: string = 'Markdown editor';
+  note: string = '';
+
+  notes: Note[] = []
+
+  constructor(private notesService: NoteService) {
+
+  }
+
+  ngOnInit() {
+    this.getAllNotes();
+  }
+
+  
+  noteChanges(value: string) : void {
+    this.note = value;
+    localStorage.setItem('note', this.note);
+  }
+  
+  createNewNote() {
+    const newNote: Note = {
+      title: `Nota ${this.notes.length +1}`,
+      value: ''
+    }
+    
+    this.notesService.saveNote(newNote);
+    this.getAllNotes();
+  }
+  
+  private getAllNotes() {
+    this.notes = this.notesService.findAll();
+  }
 }
